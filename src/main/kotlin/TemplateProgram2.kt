9f1983943d51a7cae3d1720fae5935438050255d
Edwin Jakobs <edwin@rndr.studio>
2020-04-01 21:59:25 +0200

    Add time-scratcher to TemplateProgram2.kt

--- src/main/kotlin/TemplateProgram2.kt
+++ src/main/kotlin/TemplateProgram2.kt
@@ -1,4 +1,8 @@
 import org.openrndr.application
+import org.openrndr.panel.controlManager
+import org.openrndr.panel.elements.Range
+import org.openrndr.panel.elements.Slider
+import org.openrndr.panel.elements.slider
 import org.operndr.extra.keyframer.Keyframer
 import org.operndr.extra.keyframer.KeyframerFormat
 import org.operndr.extras.filewatcher.watchFile
@@ -17,6 +21,27 @@ fun main() = application {
             val color by RGBChannel(arrayOf("r", "g", "b"))
         }
 
+        var clockOffset = 0.0
+        val oldClock = clock
+        clock = { oldClock() - clockOffset }
+
+        var clockSlider: Slider? = null
+
+        val cm = controlManager {
+            layout {
+                clockSlider = slider {
+                    range = Range(0.0, 30.0)
+                    events.valueChanged.listen {
+                        if (it.interactive) {
+                            clockOffset = oldClock() - it.newValue
+                        }
+                    }
+                }
+            }
+        }
+
+        extend(cm)
+
         val kf = Keyframe()
         watchFile(File("data/keyframes/circle-parametric-2.json")) {
             kf.loadFromJson(
@@ -30,6 +55,9 @@ fun main() = application {
         }
 
         extend {
+            clockSlider?.value = seconds
+
+
             kf(seconds)
             drawer.fill = kf.color
             drawer.circle(kf.position, kf.radius)
